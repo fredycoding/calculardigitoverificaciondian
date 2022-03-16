9450c039bae274f9ee03fb535a585332b252ae34
xbox360colombia <xbox360colombia@gmail.com>
2022-03-16 09:45:25 -0500

    Create calculodigito.py
    código del calculo

---
+++ calculodigito.py
@@ -0,0 +1,12 @@
+import string
+
+def dv(rut):
+	factores = [3, 7, 13, 17, 19, 23, 29, 37, 41, 43, 47, 53, 59, 67, 71]
+	rut_ajustado=str(rut).rjust( 15, '0')
+	s = sum(int(rut_ajustado[14-i]) * factores[i] for i in range(14)) % 11
+	if s > 1:
+		return 11 - s
+	else:
+         return s
+
+print(dv(123456789))
